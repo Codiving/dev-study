2c38474fc8ce437f658360add209c5ada2d20b83
Codiving <codivingmoon@gmail.com>
2022-08-28 14:12:57 +0900

    feat: Add generic type in Timeline

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,22 +1,24 @@
-import { FlexTimeline } from "@/src/pageLayout/FlexPageLayout";
 import { Flex } from "@/src/ui";
 import TimelineContents from "./TimelineContents";
 import TimelineTitle from "./TimelineTitle";
 
-interface Props {
-  inner?: FlexTimeline[];
-  title?: string;
+export interface TimelineProps<T> {
+  title: string;
+  inner?: TimelineProps<T>[];
+  data?: T;
 }
 
-const Timeline = (props: Props) => {
+const Timeline = <T extends any>(props: TimelineProps<T>) => {
   const { inner = [], title } = props;
 
   return (
     <Flex direction="column">
       <TimelineTitle title={title} />
       <TimelineContents>
-        {inner.map(({ title, children: inner }) => {
-          return <Timeline key={title} title={title} inner={inner} />;
+        {inner.map(({ title, inner, data }) => {
+          return (
+            <Timeline key={title} title={title} inner={inner} data={data} />
+          );
         })}
       </TimelineContents>
     </Flex>
