b1b9858ab53050a4a23bf20dd88c6ec393e228db
Codiving <codivingmoon@gmail.com>
2022-08-30 23:04:04 +0900

    chore: Add ui component in Flex

---
+++ src/pageLayout/ui.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const FlexDescription = styled("p")(() => ({
+  padding: "5px 0",
+  fontSize: 14
+}));
+
+export const FlexTypography = styled("p")(() => ({
+  padding: "5px 0"
+}));
+
+export const FlexTitle = styled("h3")(() => ({
+  fontSize: 24,
+  fontWeight: "bold"
+}));
